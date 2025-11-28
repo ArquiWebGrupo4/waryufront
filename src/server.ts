874524777adf11ea42232c19a341217c011ab4d9
DanproTDT <162157821+DanproTDT@users.noreply.github.com>
2025-11-28 18:17:00 -0500

    WIP: antes de actualizar Angular

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,13 @@
-import {
-  AngularNodeAppEngine,
-  createNodeRequestHandler,
-  writeResponseToNodeResponse,
-} from '@angular/ssr/node';
-import express, { Request, Response, NextFunction } from 'express';
-import { join } from 'node:path';
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import 'zone.js/node';
+import bootstrap from './main.server';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+async function run(): Promise<void> {
+  const port = process.env['PORT'] || 4000;
 
-const browserDistFolder = join(__dirname, '../browser');
+  // Arranca la aplicación Angular en modo SSR
+  await bootstrap({} as any);
 
-const app = express();
-const angularApp = new AngularNodeAppEngine();
+  console.log(`✅ Angular SSR server listening on http://localhost:${port}`);
+}
 
-app.use(
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: false,
-    redirect: false,
-  }),
-);
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
-});
-
-const port = process.env['PORT'] || 4000;
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
-// Exportar handler para Angular CLI / serverless
-export const reqHandler = createNodeRequestHandler(app);
+run();
